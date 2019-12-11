7fb7adb572e4a6fa902f9c9fc54365dbcabd785f
Keren Park <parkhk10@gmail.com>
2019-12-11 12:21:29 -0600

    static state display

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import ProductContainer from './components/ProductContainer';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
 
-function App() {
+
+const App = () => {
+  // static state using react hooks already here
+  const [data, setData] = useState({});
+  const products = Object.values(data);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('./data/products.json');
+      const json = await response.json();
+      setData(json);
+    };
+    fetchProducts();
+  }, []);
+
+  const items = products.map(p => <ProductContainer
+                                      pImg={p.sku}
+                                      pTitle={p.title}
+                                      pPrice={p.price}
+                                  />);
+
+  const useStyles = makeStyles(theme => ({
+    button: {
+      margin: theme.spacing(1),
+    },
+    input: {
+      display: 'none',
+    },
+    root: {
+        flexGrow: 1,
+      },
+      paper: {
+        height: 300,
+        width: 150,
+      },
+      control: {
+        padding: theme.spacing(2),
+      },
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Sick Tees Co.</h1>
+      <Grid container direction="row" justify="center" alignItems="center" className={classes.root} spacing={2}>
+        {items.map(pContainer => 
+        <Grid item xs={4}>
+          <Grid container justify="center" spacing={2}>
+            <Grid item value={2}>
+              {pContainer}
+            </Grid>
+          </Grid>
+        </Grid>)}
+      </Grid>
+
+      {/* <div class="ui grid">{items}</div> */}
     </div>
   );
-}
+};
 
 export default App;
